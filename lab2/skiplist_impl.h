56545c6f6edc02accd0eddfcca6c0cb7a50ed402
sonnykwack <159227008+sonnykwack@users.noreply.github.com>
2025-05-12 13:53:30 +0900

    Update skiplist_impl.h

--- lab2/skiplist_impl.h
+++ lab2/skiplist_impl.h
@@ -1,7 +1,8 @@
-#include "skiplist.h"
-
 #ifndef SKIPLIST_IMPL_H
 #define SKIPLIST_IMPL_H
+
+#include "skiplist.h"
+#include <pthread.h>
 
 /**
  * @brief SkipList without lock
@@ -17,11 +18,13 @@ class SkipList : public DefaultSkipList {
         SkipList(int max_level = 16, float prob = 0.5);
         // 소멸자
         ~SkipList();
-        
-        // 멤버 함수 추가 선언 가능
+
+        // DefaultSkipList에서 요구하는 오버라이드 함수
         void insert(int key, int value) override;
         int lookup(int key) override;
         void remove(int key) override;
+
+        // 결과 확인용 traversal
         void traversal(KVC* arr);
 };
 
@@ -33,49 +36,54 @@ class SkipList : public DefaultSkipList {
  */
 class CoarseSkipList : public DefaultSkipList {
     private:
-        // 멤버 변수 추가 선언 가능
-        pthread_mutex_t mutex_lock;
-        
+        pthread_mutex_t mutex_lock;  // 전체 리스트용 락
+
     public:
         // 생성자
         CoarseSkipList(int max_level = 16, float prob = 0.5);
         // 소멸자
         ~CoarseSkipList();
-        
-        // 멤버 함수 추가 선언 가능
+
+        // DefaultSkipList에서 요구하는 오버라이드 함수
         void insert(int key, int value) override;
         int lookup(int key) override;
         void remove(int key) override;
+
+        // 결과 확인용 traversal
         void traversal(KVC* arr);
 };
 
-/// @brief FineSkipList는 FineNode를 정의하여 사용하길 권장한다.
+/**
+ * @brief Fine-grained Node
+ * Node를 상속받아 락을 포함하는 구조체 정의
+ */
 struct FineNode : public Node {
-    // 멤버 변수 추가 가능
     pthread_mutex_t lock;
 };
 
 /**
  * @brief SkipList with fine-grained lock
  * SkipList 내부의 critical section을 Node 단위로 lock으로 관리한다.
- * Node를 상속받은 FineNode를 정의하여 사용하길 권장한다.
  * DefaultSkipList의 함수를 오버라이드하여, 클래스를 완성한다.
  * 구현에 필요한 멤버 변수/함수를 추가하여 구현한다.
  */
 class FineSkipList : public DefaultSkipList {
     private:
-        // 멤버 변수 추가 선언 가능
-        
+        // 멤버 변수 추가 가능
+
     public:
         // 생성자
         FineSkipList(int max_level = 16, float prob = 0.5);
         // 소멸자
         ~FineSkipList();
-        
-        // 멤버 함수 추가 선언 가능
+
+        // DefaultSkipList에서 요구하는 오버라이드 함수
         void insert(int key, int value) override;
         int lookup(int key) override;
         void remove(int key) override;
+
+        // 결과 확인용 traversal
         void traversal(KVC* arr);
 };
-#endif
+
+#endif // SKIPLIST_IMPL_H
