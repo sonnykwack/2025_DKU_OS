ca5ab880f9355e268284596e169ee0ed9936e86b
sonnykwack <159227008+sonnykwack@users.noreply.github.com>
2025-05-12 13:29:06 +0900

    Update skiplist_impl.cpp

--- lab2/skiplist_impl.cpp
+++ lab2/skiplist_impl.cpp
@@ -1,9 +1,9 @@
 /*
 *	DKU Operating System Lab
 *	    Lab2 (Concurrent Data Structure : Skip List)
-*	    Student id : 
-*	    Student name : 
-*	    Date : 
+*	    Student id : 32210214   
+*	    Student name : 곽태훈
+*	    Date : 2025-05-12
 */
 
 #include "skiplist_impl.h"
@@ -59,15 +59,85 @@ SkipList::SkipList(int max_level, float prob) : DefaultSkipList(max_level, prob)
 SkipList::~SkipList() {}
 
 void SkipList::insert(int key, int value) {
-    // TODO
+    //TODO
+    Node* update[max_level_ + 1];
+    Node* current = header_;
+
+    // 상위 레벨부터 내려오며 삽입 위치 찾기
+    for (int i = max_level_; i >= 0; --i) {
+        while (current->forward[i] != nullptr && current->forward[i]->key < key) {
+            current = current->forward[i];
+        }
+        update[i] = current;
+    }
+
+    current = current->forward[0];
+
+    // 이미 존재하는 키: 값 누적 + 업데이트 횟수 증가
+    if (current != nullptr && current->key == key) {
+        current->value += value;
+        current->upd_cnt += 1;
+    } else {
+        int new_level = random_level();
+
+        Node* new_node = new Node();
+        new_node->key = key;
+        new_node->value = value;
+        new_node->upd_cnt = 0;
+        new_node->level = new_level;
+        new_node->forward = new Node*[new_level + 1];
+
+        for (int i = 0; i <= new_level; ++i) {
+            new_node->forward[i] = update[i]->forward[i];
+            update[i]->forward[i] = new_node;
+        }
+    }
 }
 
 int SkipList::lookup(int key) {
     // TODO
+    Node* current = header_;
+
+    // 상위 레벨부터 내려오며 키 탐색
+    for (int i = max_level_; i >= 0; --i) {
+        while (current->forward[i] != nullptr && current->forward[i]->key < key) {
+            current = current->forward[i];
+        }
+    }
+
+    current = current->forward[0];
+
+    if (current != nullptr && current->key == key) {
+        return current->value;
+    } else {
+        return 0;
+    }
 }
 
 void SkipList::remove(int key) {
     // TODO
+     Node* update[max_level_ + 1];
+    Node* current = header_;
+
+    // 삭제할 위치 탐색
+    for (int i = max_level_; i >= 0; --i) {
+        while (current->forward[i] != nullptr && current->forward[i]->key < key) {
+            current = current->forward[i];
+        }
+        update[i] = current;
+    }
+
+    current = current->forward[0];
+
+    if (current != nullptr && current->key == key) {
+        for (int i = 0; i <= current->level; ++i) {
+            if (update[i]->forward[i] != current) break;
+            update[i]->forward[i] = current->forward[i];
+        }
+
+        delete[] current->forward;
+        delete current;
+    }
 }
 
 // CoarseSkipList 생성자
@@ -82,14 +152,88 @@ CoarseSkipList::~CoarseSkipList() {
 
 void CoarseSkipList::insert(int key, int value) {
     // TODO
+     pthread_mutex_lock(&mutex_lock);
+
+    Node* update[max_level_ + 1];
+    Node* current = header_;
+
+    for (int i = max_level_; i >= 0; --i) {
+        while (current->forward[i] != nullptr && current->forward[i]->key < key) {
+            current = current->forward[i];
+        }
+        update[i] = current;
+    }
+
+    current = current->forward[0];
+
+    if (current != nullptr && current->key == key) {
+        current->value += value;
+        current->upd_cnt += 1;
+    } else {
+        int new_level = random_level();
+
+        Node* new_node = new Node();
+        new_node->key = key;
+        new_node->value = value;
+        new_node->upd_cnt = 0;
+        new_node->level = new_level;
+        new_node->forward = new Node*[new_level + 1];
+
+        for (int i = 0; i <= new_level; ++i) {
+            new_node->forward[i] = update[i]->forward[i];
+            update[i]->forward[i] = new_node;
+        }
+    }
+
+    pthread_mutex_unlock(&mutex_lock);
 }
 
 int CoarseSkipList::lookup(int key) {
     // TODO
+    pthread_mutex_lock(&mutex_lock);
+
+    Node* current = header_;
+
+    for (int i = max_level_; i >= 0; --i) {
+        while (current->forward[i] != nullptr && current->forward[i]->key < key) {
+            current = current->forward[i];
+        }
+    }
+
+    current = current->forward[0];
+
+    int result = (current != nullptr && current->key == key) ? current->value : 0;
+
+    pthread_mutex_unlock(&mutex_lock);
+    return result;
 }
 
 void CoarseSkipList::remove(int key) {
     // TODO
+    pthread_mutex_lock(&mutex_lock);
+
+    Node* update[max_level_ + 1];
+    Node* current = header_;
+
+    for (int i = max_level_; i >= 0; --i) {
+        while (current->forward[i] != nullptr && current->forward[i]->key < key) {
+            current = current->forward[i];
+        }
+        update[i] = current;
+    }
+
+    current = current->forward[0];
+
+    if (current != nullptr && current->key == key) {
+        for (int i = 0; i <= current->level; ++i) {
+            if (update[i]->forward[i] != current) break;
+            update[i]->forward[i] = current->forward[i];
+        }
+        delete[] current->forward;
+        delete current;
+    }
+
+    pthread_mutex_unlock(&mutex_lock);
 }
 
 // FineSkipList 생성자
@@ -125,12 +269,119 @@ FineSkipList::~FineSkipList() {
 
 void FineSkipList::insert(int key, int value) {
     // TODO
+    Node* update[max_level_ + 1];
+    Node* current = header_;
+
+    // 헤더 락
+    pthread_mutex_lock(&((FineNode*)current)->lock);
+
+    for (int i = max_level_; i >= 0; --i) {
+        Node* next = current->forward[i];
+        while (next != nullptr && next->key < key) {
+            pthread_mutex_lock(&((FineNode*)next)->lock);
+            pthread_mutex_unlock(&((FineNode*)current)->lock);
+            current = next;
+            next = current->forward[i];
+        }
+        update[i] = current;
+    }
+
+    Node* target = current->forward[0];
+    if (target != nullptr)
+        pthread_mutex_lock(&((FineNode*)target)->lock);
+
+    if (target != nullptr && target->key == key) {
+        target->value += value;
+        target->upd_cnt += 1;
+        pthread_mutex_unlock(&((FineNode*)target)->lock);
+    } else {
+        int new_level = random_level();
+        Node* new_node = new Node();
+        new_node->key = key;
+        new_node->value = value;
+        new_node->upd_cnt = 0;
+        new_node->level = new_level;
+        new_node->forward = new Node*[new_level + 1];
+
+        // 새 노드에 락 생성
+        pthread_mutex_init(&((FineNode*)new_node)->lock, nullptr);
+
+        for (int i = 0; i <= new_level; ++i) {
+            new_node->forward[i] = update[i]->forward[i];
+            update[i]->forward[i] = new_node;
+        }
+    }
+
+    // 락 해제
+    for (int i = 0; i <= max_level_; ++i) {
+        pthread_mutex_unlock(&((FineNode*)update[i])->lock);
+    }
 }
 
 int FineSkipList::lookup(int key) {
     // TODO
+    Node* current = header_;
+    pthread_mutex_lock(&((FineNode*)current)->lock);
+
+    for (int i = max_level_; i >= 0; --i) {
+        Node* next = current->forward[i];
+        while (next != nullptr && next->key < key) {
+            pthread_mutex_lock(&((FineNode*)next)->lock);
+            pthread_mutex_unlock(&((FineNode*)current)->lock);
+            current = next;
+            next = current->forward[i];
+        }
+    }
+
+    Node* target = current->forward[0];
+    int result = 0;
+
+    if (target != nullptr) {
+        pthread_mutex_lock(&((FineNode*)target)->lock);
+        if (target->key == key) result = target->value;
+        pthread_mutex_unlock(&((FineNode*)target)->lock);
+    }
+
+    pthread_mutex_unlock(&((FineNode*)current)->lock);
+    return result;
 }
 
 void FineSkipList::remove(int key) {
     // TODO
+     Node* update[max_level_ + 1];
+    Node* current = header_;
+
+    pthread_mutex_lock(&((FineNode*)current)->lock);
+
+    for (int i = max_level_; i >= 0; --i) {
+        Node* next = current->forward[i];
+        while (next != nullptr && next->key < key) {
+            pthread_mutex_lock(&((FineNode*)next)->lock);
+            pthread_mutex_unlock(&((FineNode*)current)->lock);
+            current = next;
+            next = current->forward[i];
+        }
+        update[i] = current;
+    }
+
+    Node* target = current->forward[0];
+    if (target != nullptr) pthread_mutex_lock(&((FineNode*)target)->lock);
+
+    if (target != nullptr && target->key == key) {
+        for (int i = 0; i <= target->level; ++i) {
+            if (update[i]->forward[i] == target)
+                update[i]->forward[i] = target->forward[i];
+        }
+
+        pthread_mutex_unlock(&((FineNode*)target)->lock);
+        pthread_mutex_destroy(&((FineNode*)target)->lock);
+        delete[] target->forward;
+        delete target;
+    } else {
+        if (target != nullptr) pthread_mutex_unlock(&((FineNode*)target)->lock);
+    }
+
+    for (int i = 0; i <= max_level_; ++i) {
+        pthread_mutex_unlock(&((FineNode*)update[i])->lock);
+    }
 }
